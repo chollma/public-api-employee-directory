34bd748d5af6417204c2a67e52ea190c6d904e5f
chollma <53924848+chollma@users.noreply.github.com>
2020-10-18 13:58:26 -0700

    Working on triggering and filling the modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-// SEARCH -- doesn't interact at all with JSON response
+/******************************************
+Student: Cooper Hollmaier
+Project: Techdegree - Unit 5
+Intended Behavior: Public API Requests
+Goal: Exceeds Expecatations
+******************************************/
+
+/******************************************
+SEARCH
+******************************************/
 
 // Create form
 const searchContainer = document.querySelector('.search-container');
@@ -25,23 +34,30 @@ form.appendChild(submit);
 // Add it to the page
 searchContainer.appendChild(form);
 
-// --------------------------------------------------------
+/******************************************
+API REQUEST
+******************************************/
 
-// Fetch Request
+// Get data from the random user database
 
-fetch('https://randomuser.me/api/?results=10')
+fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
-    .then(data => generateCard(data.results)); // push data into a helper function that will create the card
+    .then(data => generateCard(data.results));
 
+/******************************************
+EMPLOYEE CARDS
+******************************************/
 
+// Create the HTML card for each object returned    
 function generateCard(data) {
+    // Put cards on the page
     for (i = 0; i < data.length; i++) {
-        // Create the card to hold the info
+
+        // Build the card
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
-        // Get thumbnail image
-        console.log();
-        // Create the image container
+        card.setAttribute('id', i);
+        // Handle the image
         const imageContainer = document.createElement('div');
         imageContainer.setAttribute('class', 'card-img-container');
         const img = document.createElement('img');
@@ -49,7 +65,7 @@ function generateCard(data) {
         img.setAttribute('src', data[i].picture.large);
         img.setAttribute('alt', 'profile picture');
         imageContainer.appendChild(img);
-
+        // Handle the user information
         const infoContainer = document.createElement('div');
         infoContainer.setAttribute('class', 'card-info-container');
         const h = document.createElement('h3');
@@ -62,31 +78,30 @@ function generateCard(data) {
         const cardTextCap = document.createElement('p');
         cardTextCap.setAttribute('class', 'card-text cap');
         cardTextCap.innerHTML = data[i].location.city + ', ' + data[i].location.state;
+        // Assemble the information card pieces
         infoContainer.appendChild(h);
         infoContainer.appendChild(cardText);
         infoContainer.appendChild(cardTextCap);
         const gallery = document.getElementById('gallery');
-
-
-
-
+        // Add the card to the page
         gallery.appendChild(card);
         card.appendChild(imageContainer);
         card.appendChild(infoContainer);
-
-
-
-
     }
 
 }
 
+/******************************************
+EMPLOYEE INFORMATION MODAL
+******************************************/
 
-// MODAL
-
+// Create the container to hold the modal object
 const modalContainer = document.createElement('div');
 modalContainer.setAttribute('class', 'modal-container');
 modalContainer.hidden = true;
+
+
+// Create the modal object
 const modalObject = document.createElement('div');
 modalObject.setAttribute('class', 'modal');
 const closeButton = document.createElement('button');
@@ -94,49 +109,49 @@ closeButton.setAttribute('type', 'button');
 closeButton.setAttribute('id', 'modal-close-btn');
 closeButton.setAttribute('class', 'modal-close-btn');
 closeButton.innerHTML = '<strong>X</strong>';
+
+// Add an event listener for the close modal button
 closeButton.addEventListener('click', () => {
     modalContainer.hidden = true;
 })
+
+// Create the modal information container
 const modalInfoContainer = document.createElement('div');
 modalInfoContainer.setAttribute('class', 'modal-info-container');
 
-// TODO img
+// Create the modal image container
 const modalImage = document.createElement('img');
 modalImage.setAttribute('class', 'modal-img');
 modalImage.setAttribute('src', 'https://placehold.it/125x125');
 modalImage.setAttribute('alt', 'profile picture');
-// TODO h3
+
+// Fill the container with data
 const modalH = document.createElement('h3');
 modalH.setAttribute('id', 'name');
 modalH.setAttribute('class', 'modal-name cap');
 modalH.innerHTML = 'name';
-// TODO p
 const modalPEmail = document.createElement('p');
 modalPEmail.setAttribute('class', 'modal-text');
 modalPEmail.innerHTML = 'email';
-// TODO p
 const modalPCity = document.createElement('p');
 modalPCity.setAttribute('class', 'modal-text cap');
 modalPCity.innerHTML = 'city';
-// TODO hr
 const modalHR = document.createElement('hr');
-// TODO p
 const modalPPhone = document.createElement('p');
 modalPPhone.setAttribute('class', 'modal-text');
 modalPPhone.innerHTML = '(555) 555-5555';
-// TODO p
 const modalPAddress = document.createElement('p');
 modalPAddress.setAttribute('class', 'modal-text');
 modalPAddress.innerHTML = '123 Portland Ave., Portland, OR 97204';
-// TODO p
 const modalPBirthday = document.createElement('p');
 modalPBirthday.setAttribute('class', 'modal-text');
 modalPBirthday.innerHTML = 'Birthday: 10/21/2015';
 
-// EXCEEDS
-// TODO Modal Button Container
+// Create an additional div to hold prev/next buttons
 const prevNextContainer = document.createElement('div');
 prevNextContainer.setAttribute('class', 'modal-btn-container');
+
+// Create the previous and next buttons
 const modalPrevBtn = document.createElement('button');
 modalPrevBtn.setAttribute('type', 'button');
 modalPrevBtn.setAttribute('id', 'modal-prev');
@@ -148,24 +163,16 @@ modalNextBtn.setAttribute('id', 'modal-next');
 modalNextBtn.setAttribute('class', 'modal-next btn');
 modalNextBtn.innerHTML = 'Next';
 
+// Assemble the modal elements and add it to the page
 
-
-// Assembly
 const modalElements = [modalImage, modalH, modalPEmail, modalPCity, modalHR, modalPPhone, modalPAddress, modalPBirthday];
 for (i = 0; i < modalElements.length; i++) {
     modalInfoContainer.appendChild(modalElements[i]);
 }
-
 prevNextContainer.appendChild(modalPrevBtn);
 prevNextContainer.appendChild(modalNextBtn);
-
 modalObject.appendChild(modalInfoContainer);
 modalObject.appendChild(closeButton);
-
 modalContainer.appendChild(modalObject);
 modalContainer.appendChild(prevNextContainer);
-
-
-
-// TODO Add it to the page
 gallery.after(modalContainer);
